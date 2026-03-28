cbcd90dcd545ba562ae0f3b4e86a4ecce4de337e
Anthony Rossa <anthonysixty23@gmail.com>
2026-03-28 10:51:03 -0300

    auth code added

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,18 +1,10 @@
 const Card = require('../models/card');
-const { ERROR_CODE_400, ERROR_CODE_404, ERROR_CODE_500 } = require('../utils/constants');
+const { ERROR_CODE_400, ERROR_CODE_404, ERROR_CODE_500, ERROR_CODE_403 } = require('../utils/constants');
 
 const getCards = (req, res) => {
   Card.find()
-  .orFail(() => {
-    const error = new Error('Cards não encontrados');
-    error.statusCode = ERROR_CODE_404;
-    throw error;
-  })
     .then(cards => res.send(cards))
     .catch(err => {
-      if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Dados do card inválidos' });
-      }
       res.status(ERROR_CODE_500).send({ message: 'Erro interno do servidor' });
     });
 };
@@ -32,17 +24,23 @@ const createCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   const { cardId } = req.params;
-  Card.findByIdAndDelete(cardId)
-  .orFail(() => {
-    const error = new Error('Card não encontrado');
-    error.statusCode = ERROR_CODE_404;
-    throw error;
-  })
+  Card.findById(cardId)
+    .orFail(() => {
+      const error = new Error('Card não encontrado');
+      error.statusCode = ERROR_CODE_404;
+      throw error;
+    })
     .then(card => {
-      if (!card) {
-        return res.status(ERROR_CODE_404).send({message: 'Card não encontrado'});
+        if (card.owner.toString() !== req.user._id) {
+          const error = new Error('Permissão negada para deletar este card');
+          error.statusCode = ERROR_CODE_403;
+          throw error;
       }
-      res.send({message: 'Card deletado com sucesso'});
+
+      return Card.findByIdAndDelete(cardId);
+    })
+    .then(() => {
+      res.send({ message: 'Card deletado com sucesso' });
     })
     .catch(err => {
       if (err.name === 'CastError') {
